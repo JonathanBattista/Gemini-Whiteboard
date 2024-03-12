8aa5b6f5b24ea578758e8ef1d1c5d82225967f8d
JonathanBattista <145586415+JonathanBattista@users.noreply.github.com>
2024-03-12 13:41:39 -0400

    added file syntax and json data.

---
+++ src/pages/Home/Homepage/Homepage.js
@@ -0,0 +1,15 @@
+import { Hero } from "../../../components/Hero/Hero";
+import { Footer } from "../../../components/Footer/Footer";
+import { Result } from "../../../components/Result/Result";
+
+const Homepage = () => {
+  return (
+    <>
+      <Hero />
+      <Result />
+      <Footer />
+    </>
+  );
+};
+
+export { Homepage };
